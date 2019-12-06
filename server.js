5aca7fd4eba533efff022c56f4b9924af8539f33
Jarrett Kong <jarrettkong@me.com>
2019-12-06 16:30:48 -0700

    Remove default files and finish twilio endpoint functionality
    Signed-off-by: Jarrett Kong <jarrettkong@me.com>

--- server.js
+++ server.js
@@ -1,13 +1,27 @@
-require('dot-env');
+require('dotenv').config();
 
-const app = require('express')();
+const express = require('express')
+const app = express();
 const serveStatic = require('serve-static');
 const port = process.env.PORT || 5000;
 
-app.use(serveStatic(__dirname + '/dist'));
+const accountSid = process.env.TWILIO_ACCOUNT_SID
+const authToken = process.env.TWILIO_AUTH_TOKEN
+
+const client = require('twilio')(accountSid, authToken)
+
+app.use(express.json())
+app.use(serveStatic(__dirname + '/build'));
 
 app.post('/sms', (req, res) => {
-  
+  client.messages.create({
+    body: req.body.message,
+    from: process.env.TWILIO_FROM_NUMBER,
+    to: process.env.TO_NUMBER
+  }).then(() => {
+    console.log('Message sent!')
+    res.sendStatus(200)
+  }).catch(err => res.status(404).json(err))
 })
 
-app.listen(port, console.log('server started ' + port));
+app.listen(port, console.log('App listening on port: ' + port));
--- src/App.js
+++ src/App.js
@@ -4,23 +4,34 @@ import './App.css';
 class App extends Component {
 
   state = {
-    message: ''
+    message: '',
+    error: ''
   }
 
-  sendSMS = e => {
+  sendSMS = async e => {
     e.preventDefault()
-    console.log(this.state.message)
-    this.setState({ message: '' })
+    try {
+      await fetch('/sms', {
+        method: 'POST',
+        headers: { 'Content-Type': 'application/json' },
+        body: JSON.stringify({ message: this.state.message })
+      })
+      this.setState({ message: '' })
+    } catch (err) {
+      this.setState({ error: err.message })
+    }
   }
 
   render() {
+    const { error } = this.state
     return (
-      <div class="App">
+      <div className="App">
         <h1>Twilio, I'd love to chat!</h1>
         <form onSubmit={this.sendSMS}>
           <input type="text" value={this.state.message} onChange={e => this.setState({ message: e.target.value })} />
           <input type="submit" value="Text me!" />
         </form>
+        <h3>{error && error}</h3>
       </div>
     )
   }
